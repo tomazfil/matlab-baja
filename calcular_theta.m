462e75a1886d1cf4850f1dbf4e0890a3e92a3e67
Tomaz Filgueira <tomaz.fnunes@gmail.com>
2017-06-21 14:15:12 -0300

    PCA

--- calcular_theta.m
+++ calcular_theta.m
@@ -1,37 +1,37 @@
-function [theta] = calcular_theta(var,k)
-% X_lat_lap1(1,1) = lat1(2);
-% X_lat_lap1(1,2) = lat1(1);
+function [theta] = calcular_theta(var,k,passo)
+theta = zeros(k,round( (length(var)-500)/passo)); %inteiro de uma divisão
+col=1;
+    for x=500:passo:length(var)
 
-%Inicio da matriz
-matriz = zeros(length(var)-k,k);
+        %Inicio em var(1:500)
+        temp = var(x-499:x);
 
-%primeira linha
-%matriz(1,:) = [var(1) 0 0 ] ;
-%Segunda Linha
-%matriz(2,:) = [var(2) var(1) 0];
-%Terceira Linha
-%matriz(3,:) = [var(3) var(2) var(1)];
 
-%linha 1 tem colunas de 1 até atraso - 1
-for i=0:k-1
-matriz(1,i+1) = var(k-i);
-end
 
-for i=2:length(var)-k+1 %3 posições a frente do inicio
-    
-    %matriz = [matriz; [var(i-1) var(i-2) var(i-3)] ];
-    %Reduzo a(3) em uma posição ->a(2), depois em outra posição a(1)
-    
-    
-     %matriz(i,1) = var(k+i-1);
-    for j=0:k-1 %atraso de zero até k-1 colunas
-        matriz(i,j+1) = var(k+i-1-j);
-    
+        %Inicio da matriz
+        matriz = zeros(length(temp)-k,k);
+
+        %linha 1 tem colunas de 1 até atraso - 1
+        for i=0:k-1
+            matriz(1,i+1) = temp(k-i);
+        end
+
+        for i=2:length(temp)-k+1 %i  linhas até tamanho de var -k + 1
+
+            %matriz = [matriz; [var(i-1) var(i-2) var(i-3)] ];
+            %Reduzo a(3) em uma posição ->a(2), depois em outra posição a(1)
+
+
+            %matriz(i,1) = var(k+i-1);
+            for j=0:k-1 % j atrasos de zero até k-1 colunas
+                matriz(i,j+1) = temp(k+i-1-j);
+
+            end
+        end
+
+        %Multiplicar theta_estimado = PINV*Acc[k]
+
+        theta(:,col) = pinv(matriz)*temp(k:length(temp)); %inicio no valor var(k)
+        col=col+1;
     end
-end
-
-%Multiplicar theta_estimado = PINV*Acc[k]
-%var é reduzido de uma unidade
-theta = pinv(matriz)*var(k:length(var));
-
 end
--- normalizar.m
+++ normalizar.m
@@ -1,65 +1,13 @@
-close all, clear all, clc, format compact
-load('Vaneli Pista 2.mat')
-
-%ba
-% long1 = L006_GPS_LonAcc;
-% long2 = R02L003_GPS_LonAcc;
-% lat1 = L006_GPS_LatAcc;
-% lat2 = R02L003_GPS_LatAcc;
-
-%vaneli
-long1 = L002_GPS_LonAcc;
-long2 = L005_GPS_LonAcc;
-lat1 = L002_GPS_LatAcc;
-lat2 = L005_GPS_LatAcc;
+function normalizado = normalizar(vetor)
 format long
-[m n1] = size(lat1);
-for i=1:n1;
-    long1(i) = (long1(i)-min(long1))/( max(long1) - min(long1));
-    
-    long2(i) = (long2(i)-min(long2))/( max(long2) - min(long2));
-end
-[m n2] = size(lat2);
-for i=1:n2;
-    lat1(i) = (lat1(i)-min(lat1))/( max(lat1) - min(lat1));
-    
-    lat2(i) = (lat2(i)-min(lat2))/( max(lat2) - min(lat2));
-end
-%primeira janela de tempo
-j=1;
-for i=500:50:length(lat1)
-   
-    theta_lat(j,:) = calcular_theta(lat1(i-499:i));
-    theta_long(j,:) = calcular_theta(long1(i-499:i));
-    
-    figure(1)
-    plot(theta_lat(j,1),theta_lat(j,2),'*');
-    hold on
-    grid on
-    
-    figure(2)
-    plot(theta_long(j,1),theta_long(j,2),'*');
-    hold on
-    grid on
-    j = j+1;
+n = length(vetor);
+normalizado = zeros(n,1);
+
+    for i=1:n;
+        normalizado(i) = (vetor(i)-min(vetor))/( max(vetor) - min(vetor));
+
+    end
+
 end
 
-%Janela de Tempo de tamanho 500 unidades.
-%Percorrendo o vetor em um passo de 50 unidades
-for i=500:50:length(lat2)
-    
-    theta_lat(j,:) = calcular_theta(lat2(i-499:i));
-    theta_long(j,:) = calcular_theta(long2(i-499:i));
-    
-    figure(1)
-    plot(theta_lat(j,1),theta_lat(j,2),'*');
-    hold on
-    grid on
-    
-    figure(2)
-    plot(theta_long(j,1),theta_long(j,2),'*');
-    hold on
-    grid on
-    j = j+1;
-end
 
